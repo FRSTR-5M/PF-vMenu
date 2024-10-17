932b441ddf8c81984c554b565276669f374afef7
BenniCubed <cubed@benni.rocks>
2024-10-17 15:51:02 +0200

    Disable Freecam2 not allowed notification

--- vMenu/Freecam2/Main.cs
+++ vMenu/Freecam2/Main.cs
@@ -38,31 +38,31 @@ namespace Freecam2
             if (IsAllowed(Permission.Freecam))
             {
                 RegisterKeyMapping($"{GetSettingsString(Setting.vmenu_individual_server_id)}vMenu:freecam", "vMenu Freecam Toggle Button", "keyboard", FreecamKey);
-            }
-            RegisterCommand($"{GetSettingsString(Setting.vmenu_individual_server_id)}vMenu:freecam", new Action<int, List<object>, string>((source, args, raw) =>
-            {
-                if (!MainMenu.vMenuEnabled)
-                    return;
+                RegisterCommand($"{GetSettingsString(Setting.vmenu_individual_server_id)}vMenu:freecam", new Action<int, List<object>, string>((source, args, raw) =>
+                {
+                    if (!MainMenu.vMenuEnabled)
+                        return;
 
-                if (IsAllowed(Permission.Freecam))
-                {
-                    IsInFreecam = !IsInFreecam;
-                    if (IsInFreecam)
+                    if (IsAllowed(Permission.Freecam))
                     {
-                        //TriggerEvent("mosh_notify:notify", "INFO", "<span class=\"text-black\">You are now using Freecam²!</span>", "info", "info", 5000);
-                        Notify.Success("You are now using Freecam²!");
-                        Freecam.Enable();
+                        IsInFreecam = !IsInFreecam;
+                        if (IsInFreecam)
+                        {
+                            //TriggerEvent("mosh_notify:notify", "INFO", "<span class=\"text-black\">You are now using Freecam²!</span>", "info", "info", 5000);
+                            Notify.Success("You are now using Freecam²!");
+                            Freecam.Enable();
+                        }
+                        else
+                            Freecam.Disable();
                     }
-                    else
-                        Freecam.Disable();
-                }
-                else if (!freecamNotAllowedNotified)
-                {
-                    //TriggerEvent("mosh_notify:notify", "Error", "<span class=\"text-black\">You are not allowed to use Freecam²!</span>", "error", "error", 5000);
-                    Notify.Error("You are not allowed to use Freecam²! Consider removing this key bind (Settings > Key Bindings > FiveM) if you do not wish to see this message again.");
-                    freecamNotAllowedNotified = true;
-                }
-            }), false);
+                    else if (!freecamNotAllowedNotified)
+                    {
+                        //TriggerEvent("mosh_notify:notify", "Error", "<span class=\"text-black\">You are not allowed to use Freecam²!</span>", "error", "error", 5000);
+                        Notify.Error("You are not allowed to use Freecam²! Consider removing this key bind (Settings > Key Bindings > FiveM) if you do not wish to see this message again.");
+                        freecamNotAllowedNotified = true;
+                    }
+                }), false);
+            }
         }
 
         public static void SendBasicMessage(string message)
